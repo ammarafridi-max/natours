1b4f0c2a5898c03dfd5b670a8f79fca5dc9a5d43
Ammar Afridi <ammar.afridi95@gmail.com>
2025-02-27 17:16:14 +0400

    Removed server.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
@@ -17,13 +19,26 @@ const reviewRouter = require('../routes/reviewRoutes');
 const bookingRouter = require('../routes/bookingRoutes');
 const viewsRouter = require('../routes/viewRoutes');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
 const app = express();
 
-app.enable('trust proxy');
-
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(
   helmet({
@@ -96,5 +111,25 @@ app.all('*', (req, res, next) => {
 });
 
 app.use(globalErrorHandler);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
 
 module.exports = app;
